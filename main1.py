a64e646bb117281cd062f011542b3906085baf72
Denis Novikov <dnovikov2105@gmail.com>
2024-12-06 17:20:08 +0100

    changed 'game over' logic

--- main1.py
+++ main1.py
@@ -7,6 +7,7 @@ class App:
     def __init__(self):
         pyxel.init(240, 128)
 
+        self.game_paused = False
         self.ghosts = [
             Ghost(32, 32, pyxel.COLOR_RED),
             Ghost(64, 32, pyxel.COLOR_CYAN),
@@ -43,7 +44,7 @@ class App:
             big_dot_color = pyxel.COLOR_RED,
             wall_color = pyxel.COLOR_DARK_BLUE,
         )
-        self.pacman = Pacman(x = 16, y = 16, speed = 2, color = pyxel.COLOR_YELLOW)
+        self.pacman = Pacman(x = 200, y = 16, speed = 2, color = pyxel.COLOR_YELLOW)
         self.score = 0
         
         print(f"Pacman initialized at ({self.pacman.x}, {self.pacman.y})")
@@ -51,12 +52,20 @@ class App:
         pyxel.run(self.update, self.draw)
 
     def update(self):
+        if self.game_paused:
+            # show 'game over' text on the screen (in draw method)
+                if pyxel.btnp(pyxel.KEY_R):
+                    self.restart_game()
+                elif pyxel.btnp(pyxel.KEY_Q):                        
+                    pyxel.quit()
+                return
+                    
         self.pacman.handle_input()
         self.pacman.move(self.maze.maze_layout)
         
         # Update ghosts
         for ghost in self.ghosts:
-            ghost.move(self.maze.maze_layout)
+            ghost.move(self.maze.maze_layout, self.pacman.x, self.pacman.y)
             
         for ghost in self.ghosts:
             if abs(ghost.x - self.pacman.x) < 8 and abs(ghost.y - self.pacman.y) < 8:
@@ -64,8 +73,10 @@ class App:
                     ghost.alive = False  # ghost is eaten
                     self.score += 200   # add points for eating a ghost
                 else:
-                    pyxel.quit() # end game
-
+                    self.game_paused = True
+                    break
+                    
+                    
         # Collision detection with dots
         score_increment = self.maze.check_dot_collision(self.pacman.x // 8, self.pacman.y // 8, self.pacman)
         self.score += score_increment
@@ -83,6 +94,25 @@ class App:
                 self.pacman.x, self.pacman.y = 16, 16
             else:
                 pyxel.quit()
+                
+    def restart_game(self):
+        self.current_maze_index = 0
+        self.maze = Maze(
+            maze_layout = self.mazes[self.current_maze_index],
+            dot_color=pyxel.COLOR_YELLOW,
+            big_dot_color = pyxel.COLOR_RED,
+            wall_color = pyxel.COLOR_DARK_BLUE,
+        )
+        self.pacman = Pacman(x = 200, y = 16, speed = 2, color = pyxel.COLOR_YELLOW)
+        self.score = 0
+        
+        self.ghosts = [
+            Ghost(32, 32, pyxel.COLOR_RED),
+            Ghost(64, 32, pyxel.COLOR_CYAN),
+            Ghost(32, 64, pyxel.COLOR_ORANGE),
+            Ghost(64, 64, pyxel.COLOR_PINK),
+        ]
+        self.game_paused = False
 
     def draw(self):
         pyxel.cls(0)
@@ -98,6 +128,11 @@ class App:
         
         if self.pacman.power_up_active:
             pyxel.text(5, 20, "Power up active", pyxel.COLOR_GREEN)
+        
+        # draw 'Game Over' text
+        if self.game_paused:
+            pyxel.text(50, 60, 'GAME OVER', pyxel.COLOR_RED)
+            pyxel.text(35, 80, "PRESS 'R' TO RESTART OR 'Q' TO QUIT", pyxel.COLOR_WHITE)
 
 
 App()
